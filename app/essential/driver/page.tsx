b68e2da30587e62e6af6c7fb85fa01e0392778ca
Proo2005 <prochak1922@gmail.com>
2026-01-25 20:53:55 +0530

    driver

--- app/essential/driver/page.tsx
+++ app/essential/driver/page.tsx
@@ -82,9 +82,9 @@ export default function DriversPage() {
 
   return (
     <div className="min-h-screen bg-gradient-to-b from-[#F7F9FC] to-[#EEF2F7] pb-16 px-4 relative -mt-24 text-black">
-      <div className="max-w-6xl mx-auto  pt-32 ">
-      {/* Header */}
       
+      {/* Header */}
+      <div className="flex justify-between items-center mb-8 pt-32">
         <h1 className="text-3xl font-bold">Drivers</h1>
         <button
           onClick={() => setShowForm(true)}
