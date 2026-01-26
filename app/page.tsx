dea29b54a7a531cabd3f4cbe039bd114466ef83f
Proo2005 <prochak1922@gmail.com>
2026-01-26 15:58:37 +0530

    admin

--- app/page.tsx
+++ app/page.tsx
@@ -117,7 +117,7 @@ export default function HomePage() {
   /* ================= UI ================= */
 
   return (
-    <div className="min-h-screen bg-gradient-to-b from-[#F7F9FC] to-[#EEF2F7] pb-20 px-4 -mt-24">
+    <div className="min-h-screen bg-gradient-to-b from-[#F7F9FC] to-[#EEF2F7] pb-20 px-4 -mt-24 text-black">
       <div className="max-w-7xl mx-auto grid grid-cols-1 lg:grid-cols-[260px_1fr] gap-8 pt-32">
 
         {/* ============ FILTER PANEL ============ */}
