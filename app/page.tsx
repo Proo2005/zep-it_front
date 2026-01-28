447f49c578b5dbe20a4fc28a6f65e0f68725b47b
Proo2005 <prochak1922@gmail.com>
2026-01-28 18:01:19 +0530

    password

--- app/page.tsx
+++ app/page.tsx
@@ -130,7 +130,7 @@ export default function HomePage() {
   });
 
   return (
-    <div className="min-h-screen bg-gradient-to-b from-[#F7F9FC] to-[#EEF2F7] pb-16 px-4 relative -mt-24 text-black">
+    <div className="min-h-screen bg-gradient-to-b from-[#F7F9FC] to-[#EEF2F7] pb-16 px-4 relative -mt-24 text-black dark:bg-zinc-900 dark:text-white">
       <div className="max-w-7xl mx-auto grid grid-cols-1 lg:grid-cols-[260px_1fr] gap-8 pt-32">
 
         {/* FILTER PANEL */}
